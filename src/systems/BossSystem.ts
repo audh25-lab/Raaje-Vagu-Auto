20e921fc1504e789c4362f51ed2ea04523f0c26e
audh25-lab <audh25@gmail.com>
2025-12-17 23:35:02 +0500

    Update BossSystem.ts

--- src/systems/BossSystem.ts
+++ src/systems/BossSystem.ts
@@ -1,16 +1,31 @@
-import Phaser from 'phaser';
-import PressureSystem from './PressureSystem';
+import PoliceSystem from './PoliceSystem';
 import GangSystem from './GangSystem';
+import Phaser from 'phaser';
 
-export default class BossSystem {
+export default class MBHSystem {
+  private active = false;
   private scene: Phaser.Scene;
-  private gangs: GangSystem;
+  private eliteUnits: Phaser.Physics.Arcade.Group;
 
-  constructor(scene: Phaser.Scene, gangs: GangSystem) {
+  constructor(scene: Phaser.Scene) {
     this.scene = scene;
-    this.gangs = gangs;
+    this.eliteUnits = this.scene.physics.add.group();
   }
 
-  checkSpawnConditions(pressure: PressureSystem, gangs: GangSystem) {
+  activate() { this.active = true; }
+
+  overrideIfActive(police: PoliceSystem, gangs: GangSystem) {
+    if (this.active) {
+      police['units'].getChildren().forEach(u => (u.body as Phaser.Physics.Arcade.Body).setVelocity(300));
+      if (Math.random() < 0.05) {
+        const elite = this.eliteUnits.create(Phaser.Math.Between(0, 8000), Phaser.Math.Between(0, 6000), 'elite');
+        this.scene.physics.moveToObject(elite, this.scene['player'], 250);
+      }
+      this.scene['shopsSafehouses'].safehouses.forEach(s => s.setVisible(false));
+    }
+  }
+
+  isDefeated() {
+    return this.eliteUnits.getLength() === 0 && !gangs.gangs.get('MBH')?.boss.alive;
   }
 }
