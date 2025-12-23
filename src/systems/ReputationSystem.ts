eb30da53ff694f7e15bcac2a49e7668da019d435
audh25-lab <audh25@gmail.com>
2025-12-24 00:05:04 +0500

    Delete src directory

--- src/systems/ReputationSystem.ts
+++
@@ -1,6 +0,0 @@
-export default class ReputationSystem {
-  private rep: number = 0;
-  accumulate(delta: number) { this.rep += delta; }
-  get() { return this.rep; }
-  load(rep: number) { this.rep = rep; }
-}
